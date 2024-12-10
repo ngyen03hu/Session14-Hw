d8625eee0928fc6a79f981de6f43a0b4006b2edf
ngyen03hu <nguyen33hu@gmail.com>
2024-12-10 14:51:32 +0700

    Add files via upload

---
+++ Session14-B1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (){
+	char c[1000];
+	printf("Moi ban nhap ky tu tuy y");
+	scanf("%s",&c);
+	printf("%s",c);
+	
+	
+	return 0;
+}
---
+++ Sessiono14-B2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+ 
+int main(){
+	char c[1000];
+	printf("MOi ban nhap ky tu ");
+	gets(c);
+	printf("ky tu moi nhap %s\n",c); 
+	
+	return 0;
+}
